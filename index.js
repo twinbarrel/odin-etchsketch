4b1d69bed68540102c854c884769299af279cac9
Andrew Tan <andrewsiqi@gmail.com>
2024-04-03 23:54:27 +0100

    created colorful grid

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function createGrid(width) {
     let container = document.querySelector(".container");
     for (let i=0;i<width;i++) {
         let row = document.createElement("div");
-        row.classList.add('.row');
+        row.classList.add('row');
         createRowCells(row, width);
         container.appendChild(row); 
     }
@@ -17,8 +17,33 @@ function createRowCells(row, width) {
 
 function createCell() {
     let square = document.createElement("div");
-    square.classList.add('.cell');
+    square.classList.add('cell');
     return square;
 }
 
+function generateRGB() {
+    let red = Math.floor(Math.random() * 255);
+    let green = Math.floor(Math.random() * 255);
+    let blue = Math.floor(Math.random() * 255);
+    return `rgb(${red},${blue},${green})`;
+}
+
 createGrid(16);
+
+let container = document.querySelector(".container");
+container.addEventListener("mouseover", (event)=> {
+    let target = event.target;
+    if (target.classList.contains('cell')) {
+        // target.classList.add('full');
+        let color = generateRGB();
+        target.style.backgroundColor = color;
+    }
+})
+
+container.addEventListener("mouseout", (event)=> {
+    let target = event.target;
+    if (target.classList.contains('cell')) {
+        // target.classList.remove('full');
+        target.style.backgroundColor = 'white';
+    }
+})
